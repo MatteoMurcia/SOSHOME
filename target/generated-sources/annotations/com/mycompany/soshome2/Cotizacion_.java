74b5b2dbb6ad5d799b098a9833faab8fe1e96ebf
MatteoMurcia <47997391+MatteoMurcia@users.noreply.github.com>
2019-03-21 22:34:58 -0500

    Servlet de Registro del Proveedor completo y provado

--- target/generated-sources/annotations/com/mycompany/soshome2/Cotizacion_.java
+++ target/generated-sources/annotations/com/mycompany/soshome2/Cotizacion_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-21T22:17:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-21T22:30:55")
 @StaticMetamodel(Cotizacion.class)
 public class Cotizacion_ { 
 
