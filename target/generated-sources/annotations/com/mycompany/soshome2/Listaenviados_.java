0585b0f69c992a302457e91d955d3606dd007964
MatteoMurcia <47997391+MatteoMurcia@users.noreply.github.com>
2019-03-28 10:29:26 -0500

    get

--- target/generated-sources/annotations/com/mycompany/soshome2/Listaenviados_.java
+++ target/generated-sources/annotations/com/mycompany/soshome2/Listaenviados_.java
@@ -1,17 +1,15 @@
 package com.mycompany.soshome2;
 
-import com.mycompany.soshome2.Proveedor;
-import com.mycompany.soshome2.Servicio;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-25T18:40:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-28T10:12:34")
 @StaticMetamodel(Listaenviados.class)
 public class Listaenviados_ { 
 
-    public static volatile SingularAttribute<Listaenviados, Servicio> idservice;
-    public static volatile SingularAttribute<Listaenviados, Proveedor> cedulap;
+    public static volatile SingularAttribute<Listaenviados, String> idservice;
+    public static volatile SingularAttribute<Listaenviados, String> cedulap;
     public static volatile SingularAttribute<Listaenviados, Integer> id;
 
 }
